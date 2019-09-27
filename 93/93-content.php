ee41697df90abff3e66813acbd684b44d476f9d0
Nguyễn Duy Cường <33917343+DuyCuongDZ@users.noreply.github.com>
2019-09-27 23:30:17 +0700

    changes

--- 93/93-content.php
+++ 93/93-content.php
@@ -4,6 +4,13 @@
     <head>
         <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
         <title></title>
         <?php
         if (!class_exists('lessc')) {
@@ -21,67 +28,73 @@
     <body class="type-93 home page page-id-299 page-template page-template-page-templates page-template-template-fullwidth-not-header page-template-page-templatestemplate-fullwidth-not-header-php wpb-js-composer js-comp-ver-4.10 vc_responsive">
         <div>
 
-           
+
+            
 
             <div class="wrapper">
                 <nav class="top-menu">
                     <div class="nav-inner">
                         <div class="container">
-                            <div class="row">
+                            <div class="row">   
                                 <div class="col-md-12">
+                                      <div class="navbar-header">
+                                        <button class="navbar-toggle " type="button" data-toggle="collapse" data-target=".js-navbar-collapse" style="background-color: #2c3547">
+                                            <span class="sr-only">Toggle navigation</span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                        </button>
+                                    </div>
                                     <div id="mobnav-btn"></div>
-                                    <ul data-breakpoint="800" id="menu-main-menu" class="sf-menu"><li id="menu-item-332" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-ancestor current-menu-parent menu-item-has-children menu-item-332"><a href="#">Home</a>
+                                    <ul class="sf-menu"><li><a href="#">Home</a>
                                             <ul class="sub-menu">
-                                                <li id="menu-item-301" class="menu-item   current-menu-item page_item page-item-299 current_page_item menu-item-301"><a href="#">With Revolution</a></li>
-                                                <li id="menu-item-288" class="menu-item   menu-item-288"><a href="#">With Search Bar</a></li>
-                                                <li id="menu-item-289" class="menu-item   menu-item-289"><a href="#">With Subscription</a></li>
-                                                <li id="menu-item-537" class="menu-item   menu-item-537"><a href="#">With Video</a></li>
+                                                <li ><a href="#">With Revolution</a></li>
+                                                <li ><a href="#">With Search Bar</a></li>
+                                                <li ><a href="#">With Subscription</a></li>
+                                                <li ><a href="#">With Video</a></li>
                                             </ul>
                                         </li>
-                                        <li id="menu-item-253" class="menu-item   menu-item-253"><a href="#">About us</a></li>
-                                        <li id="menu-item-255" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-255"><a href="#">Course</a>
-                                            <ul class="sub-menu">
-                                                <li id="menu-item-247" class="menu-item   menu-item-247"><a href="#">Courses Grid</a></li>
-                                                <li id="menu-item-246" class="menu-item   menu-item-246"><a href="#">Courses List</a></li>
-                                                <li id="menu-item-416" class="menu-item  menu-item-object-course menu-item-416"><a href="#">Course Details v1</a></li>
-                                                <li id="menu-item-415" class="menu-item  menu-item-object-course menu-item-415"><a href="#">Course Details v2</a></li>
-                                            </ul>
-                                        </li>
-                                        <li id="menu-item-256" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-256 menu-item-mega"><a href="#">Pages (Megamenu)</a><div class="mega-menu-container container" style="width:100%">
+                                        <li ><a href="#">About us</a></li>
+                                        <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children  menu-item-mega"><a href="#">Pages (Megamenu)</a>
+                                            <div class="mega-menu-container container" style="width:100%">
                                                 <div class="mega-menu-columns">
-                                                    <div id="menu-item-448" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-448 mega-sub-menu col-md-4"><a>Communicate</a>
+                                                    <div class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children  mega-sub-menu col-md-4"><a>Communicate</a>
                                                         <ul class="sub-menu">
-                                                            <li id="menu-item-248" class="menu-item   menu-item-248"><a href="#">Teacher</a></li>
-                                                            <li id="menu-item-291" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-291"><a href="#">Teacher profile</a></li>
-                                                            <li id="menu-item-554" class="menu-item   menu-item-554"><a href="#">Login / Register</a></li>
-                                                            <li id="menu-item-609" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-609"><a href="#">Events</a></li>
+                                                            <li class="menu-item "><a href="#">Teacher</a></li>
+                                                            <li  class="menu-item menu-item-type-custom menu-item-object-custom "><a href="#">Teacher profile</a></li>
+                                                            <li " class="menu-item   "><a href="#">Login / Register</a></li>
+                                                            <li  class="menu-item menu-item-type-custom menu-item-object-custom "><a href="#">Events</a></li>
                                                         </ul>
-                                                    </div>	<div id="menu-item-449" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-449 mega-sub-menu col-md-4"><a>Other Pages</a>
+                                                    </div>  
+                                                    <div  class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children  mega-sub-menu col-md-4"><a>Other Pages</a>
                                                         <ul class="sub-menu">
-                                                            <li id="menu-item-251" class="menu-item   menu-item-251"><a href="#">Prices and Plans</a></li>
-                                                            <li id="menu-item-252" class="menu-item   menu-item-252"><a href="#">Gallery Page</a></li>
-                                                            <li id="menu-item-250" class="menu-item   menu-item-250"><a href="#">Shortcodes</a></li>
-                                                            <li id="menu-item-455" class="menu-item   menu-item-455"><a href="#">Contact</a></li>
+                                                            <li  class="menu-item   "><a href="#">Prices and Plans</a></li>
+                                                            <li class="menu-item   "><a href="#">Gallery Page</a></li>
+                                                            <li  class="menu-item  "><a href="#">Shortcodes</a></li>
+                                                            <li  class="menu-item   "><a href="#">Contact</a></li>
                                                         </ul>
-                                                    </div>	<div id="menu-item-450" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-450 mega-sub-menu col-md-4"><a>Submenu with icons</a>
+                                                    </div>  
+                                                    <div  class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children  mega-sub-menu col-md-4"><a>Submenu with icons</a>
                                                         <ul class="sub-menu">
-                                                            <li id="menu-item-451" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-451"><a href="#">Downloads<i class="fa fa-download"></i></a></li>
-                                                            <li id="menu-item-452" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-452"><a href="#">Video files<i class="fa fa-video-camera"></i></a></li>
-                                                            <li id="menu-item-453" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-453"><a href="#">Audio files<i class="fa fa-microphone"></i></a></li>
-                                                            <li id="menu-item-454" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-454"><a href="#">Members<i class="fa fa-user"></i></a></li>
+                                                            <li  class="menu-item menu-item-type-custom menu-item-object-custom "><a href="#">Downloads<i class="fa fa-download"></i></a></li>
+                                                            <li  class="menu-item menu-item-type-custom menu-item-object-custom "><a href="#">Video files<i class="fa fa-video-camera"></i></a></li>
+                                                            <li  class="menu-item menu-item-type-custom menu-item-object-custom "><a href="#">Audio files<i class="fa fa-microphone"></i></a></li>
+                                                            <li class="menu-item menu-item-type-custom menu-item-object-custom "><a href="#">Members<i class="fa fa-user"></i></a></li>
                                                         </ul>
                                                     </div>
                                                 </div>
                                             </div>
-                                         </li>
-                                            </div><li id="menu-item-74" class="menu-item   menu-item-74"><a href="#">Blog</a></li>
-                                        <li id="menu-item-73" class="menu-item   menu-item-73"><a href="#">Contact</a></li>
+                                        </li>
+                                            <li class="menu-item  "><a href="#">Blog</a></li>
+                                        <li  class="menu-item  "><a href="#">Contact</a></li>
                                     </ul>                        
                                     <div class="col-md-3 pull-right hidden-sm hidden-xs">
                                         <div id="sb-search" class="sb-search">
-                                            <form action=#">
-                                                
-                                                <span class="fa fa-search"></span>
+                                            <form class="search">
+                                              <div class="search__wrapper">
+                                                <input type="text" name="" placeholder="Search for..." class="search__field">
+                                                <a href="#"> <button type="submit" class="fa fa-search search__icon"></button></a>
+                                              </div>
                                             </form>
                                         </div>
                                     </div>
@@ -93,6 +106,14 @@
                 <!-- content close -->
             </div>
         </div> 
+        <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
         <script src="js/superfish.js" type="text/javascript"></script>
         <script src="js/custom.js" type="text/javascript"></script>
     </body>
